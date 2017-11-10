4e9309a07e7f56fb0e3dd35aecb5d4ca6fc596a1
rjs5176 <rjs5176@rit.edu>
2017-11-10 13:40:48 -0500

    Modifications to Users class

--- capstonetracker/User.java
+++ capstonetracker/User.java
@@ -15,21 +15,39 @@ public class User
    public String lName;
    public String email;
    public String userType;
-   public String projectName;
-   public int projectID;
-   public String description;
+   private boolean canEdit;
    
-   // Constructor
-   public User()
+   //Basic default constructor - Just takes username and pass
+   public User(String _userName, String _password){
+      userName = _userName;
+      password = _password;
+      fName = "NULL";
+      lName = "NULL";
+      email = "NULL";
+      userType = "NULL";
+      canEdit = false;
+   }
+   
+   // Full parameterized Constructor - Info for either existing or current record
+   public User(String _userName, String _password, String _fName, String _lName, String _email, String _userType)
    {
-      
+      userName = _userName;
+      password = _password;
+      fName = _fName;
+      lName = _lName;
+      email = _email;
+      userType = _userType;
+      if(userType.equalsIgnoreCase("Faculty")){
+         canEdit = true;
+      }else{
+         canEdit = false;
+      }
    }
    
    // registerAccount method allow user to create new account, it can be either student or faculty
-   // Data need: userName, password, fName, lName, email, userType
-   public void registerAccount(String userName, String password, String fName, String lName, String email, String userType)
+   public void registerAccount()
    {
-      
+      //Prepare statement insert record into Users table
    }
    
    // login method allow user login to their account
@@ -37,6 +55,7 @@ public class User
    // Data need: userName, password
    public boolean login(String userName, String password)
    {
+      //Select statement on DB, if record found fill remaining varables and return true
       return true;
    }
    
@@ -45,9 +64,9 @@ public class User
    // if faculty: setEditable(true); Facultys are allow to change everything on GUI
 	//	if student: setEditable(false), Students are not able to change everything on GUI 
    // Data need: userType
-   public void checkIdentity(String userType)
+   public boolean checkPermession()
    {
-   
+      return canEdit;
    }
    
    // newChange method will save all the change that faculty make on GUI, and update the database
@@ -57,20 +76,5 @@ public class User
    {
    
    }
-   
-   // modifyProject method allow faculty adding more project 
-   // Data need: projectID, projectName
-   public void modifyProject(String projectID, String projectName)
-   {
-      
-   }
-   
-   // updateProjectInfo method allow faculty update the project information
-   // Data need: projectID, projectName, description
-   public void updateProjectInfo(String projectID, String projectName, String description)
-   {
-      
-   }
-   
    
 }
