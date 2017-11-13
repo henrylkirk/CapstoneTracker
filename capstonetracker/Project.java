de6089447004b585f5ef4ea09daa27fd0830032e
henrylkirk <henry@the-kirks.com>
2017-11-13 17:49:14 -0500

    Commented out getProjectUsers

--- capstonetracker/Project.java
+++ capstonetracker/Project.java
@@ -23,7 +23,7 @@ import java.util.*;
    private int plagiarismScore;
    private String changeTo;
    private String partOfChange;
-   private ArrayList<String> users;
+   private ArrayList<User> users;
    private connectDB dbConn;
    
    /**
@@ -132,11 +132,12 @@ import java.util.*;
    }
 
    /**
-    * 
+    * Returns an ArrayList of users associated with this project.
+    * @return ArrayList
     */
-   public ArrayList<String> getProjectUsers(){
-
-   }
+   // public ArrayList<User> getProjectUsers(){
+   //    return users;
+   // }
    
    /**
     * Only faculty are allow to use this method, student are not allow
