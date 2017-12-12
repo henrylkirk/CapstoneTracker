da7d8bbcbf98484cd26ec844847a6139f2b997d0
rjs5176 <rjs5176@rit.edu>
2017-12-11 19:48:50 -0500

    Status Class Commented

--- capstonetracker/Status.java
+++ capstonetracker/Status.java
@@ -17,6 +17,12 @@ public class Status{
    private String comment;
    private ConnectDB dbConn;
 
+   /**
+    * Constructor for a Status Object
+    * @param _statusId The status Id for this status object
+    * @param _dateModified The date when this status was assigned to a project
+    * @param _comment A comment about the status that was added
+    */
    public Status(int _statusId, String _dateModified, String _comment){
       statusId = _statusId;
       lastDateModified = _dateModified;
@@ -25,7 +31,11 @@ public class Status{
       statusDesc = "NULL";
       dbConn = new ConnectDB();
    }
-
+   
+   /**
+    * Method that Retrieves info for the status object
+    * @return boolean that determines if the check was successful
+    */
    public boolean checkStatusInfo(){
       String query = "SELECT name, description FROM status WHERE sid = ? ;";
       boolean gotStatus = false;
@@ -49,7 +59,11 @@ public class Status{
       return gotStatus;
    }
 
-
+   /**
+    * Method that updates the status for a project
+    * @param _pid The projectID for the project being updated
+    * @return boolean that determines if the operation was successful
+    */
    public boolean updateProjectStatus(String _pid){
       String query = "INSERT INTO project_status(sid,pid,last_modified,comment) VALUES(?,?,?,?);";
       String project = _pid;
@@ -69,43 +83,82 @@ public class Status{
       return updatePushed;
    }
 
-
+   /**
+    * Mutator to modify statusID of this instance
+    * @param _id the status id for this object
+    */
    public void setStatusId(int _id){
       statusId = _id;
    };
 
+   /**
+    * Mutator to modify statusName
+    * @param _name the name of this status
+    */
    public void setStatusName(String _name){
       statusName = _name;
    };
 
+   /**
+    * Mutator to modify the Description for the status
+    * @param _desc The description of this status
+    */
    public void setStatusDescription(String _desc){
       statusDesc = _desc;
    };
 
+   /**
+    * Mutator to modify the date when this status was assigned to a project
+    * @param _date The date the status was assigned to a project
+    */
    public void setDateModified(String _date){
       lastDateModified = _date;
    };
 
+   /**
+    * Mutator to modify the comment appended when updating the status for a project
+    * @param _comment The comment to add 
+    */
    public void setComment(String _comment){
       comment = _comment;
    };
 
+   /**
+    * Method that returns the statusID
+    * @return the statusID
+    */
    public int getStatusId(){
       return statusId;
    }
 
+   /**
+    * Method that returns the status Name
+    * @return the status name
+    */
    public String getStatusName(){
       return statusName;
    }
 
+   /**
+    * Method that returns the status description
+    * @return the status description
+    */
    public String getStatusDescription(){
       return statusDesc;
    }
 
+   /**
+    * Method that returns the date the status was assigned to a project
+    * @return the date when the status was assigned to a project
+    */
    public String getLastDateModified(){
       return lastDateModified;
    }
 
+   /**
+    * Method that returns the comment the was added alongside the status for a project
+    * @return the comment added with the status for a project
+    */
    public String getComment(){
       return comment;
    }
